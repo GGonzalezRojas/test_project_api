0badfa1aa85973523ad3aeaa71bf32f1f1a2b724
GGonzalezRojas <gustavo.gonzalez.r@hotmail.com>
2020-04-17 01:38:33 -0400

    Modify person model for verify rut and more large course name

--- test_23p/people_manager/models.py
+++ test_23p/people_manager/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    rut = models.CharField(max_length=10, null=False, unique=True, default='111111111-1')
+    rut = models.CharField(max_length=10, null=False, unique=True)
     name = models.CharField(max_length=50, null=False)
     lastName = models.CharField(max_length=50, null=False)
     age = models.PositiveIntegerField(null=True, blank=True)
-    course = models.CharField(max_length=10, null=False)
+    course = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return '{0},{1}'.format(self.name, self.lastName)
