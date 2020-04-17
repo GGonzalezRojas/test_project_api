f19db4c7bee0d91565b3c4cab61d1c05fa7f1158
GGonzalezRojas <gustavo.gonzalez.r@hotmail.com>
2020-04-17 00:26:29 -0400

    Create Person model and first migration

--- test_23p/people_manager/models.py
+++ test_23p/people_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    rut = models.CharField(max_length=10, null=False, unique=True, default='111111111-1')
+    name = models.CharField(max_length=50, null=False)
+    lastName = models.CharField(max_length=50, null=False)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    course = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return '{0},{1}'.format(self.name, self.lastName)
